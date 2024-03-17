4d377bf868e501abb718b4f75098220ec0c60125
Prakhar Sinha <prakhar.sinha2k2@gmail.com>
2024-03-17 18:12:14 +0530

    started writing the routes, written authentication routes and added jwt

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,60 @@
 import { Hono } from 'hono';
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { sign} from 'hono/jwt'
 
-// Create the main Hono app
-const app = new Hono();
+//To get the right types on c.env, when initializing the Hono app, pass the types of env as a generic
+const app = new Hono<{
+	Bindings: {
+		DATABASE_URL: string,
+    JWT_SECRET: string,
+	}
+}>();
 
-app.post('/api/v1/signup', (c) => {
-	return c.text('signup route')
+app.get('/api/v1/signup',async (c) => {
+    const prisma = new PrismaClient({
+      datasourceUrl: c.env?.DATABASE_URL,        //c is the super power in honoJS
+    }).$extends(withAccelerate())
+
+    const body=await c.req.json()           //remember app.use(express.json()), this line serves the same purpose
+    try {
+      const user = await prisma.user.create({
+        data: {
+          name: body.name,
+          email: body.email,
+          password: body.password
+        }
+      });
+    
+      return c.text('User Registered Successfully!')
+    } catch(e) {
+      return c.status(403);
+    }
+
 })
 
-app.post('/api/v1/signin', (c) => {
-	return c.text('signin route')
+app.post('/api/v1/signin',async (c) => {
+    const prisma = new PrismaClient({
+      datasourceUrl: c.env?.DATABASE_URL,        
+    }).$extends(withAccelerate())
+
+    const body=await c.req.json();
+    const user=await prisma.user.findUnique({
+      where:{
+        email:body.email
+      }
+    })
+
+    if(!user)
+    {
+        c.status(403)
+        return c.json({
+          error:"User Not Found!"
+        })
+    }
+
+    const token=await sign({id:user.id},c.env.JWT_SECRET)
+    return c.json({token})
 })
 
 app.get('/api/v1/blog/:id', (c) => {
