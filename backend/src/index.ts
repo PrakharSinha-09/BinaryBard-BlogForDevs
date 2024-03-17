aa332f80079994f647a30fcfbef0595f2415e28e
Prakhar Sinha <prakhar.sinha2k2@gmail.com>
2024-03-17 23:14:26 +0530

    some little fixes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,8 @@
 import { Hono } from 'hono';
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
-import { sign} from 'hono/jwt'
+import { sign } from 'hono/jwt'
 
-//To get the right types on c.env, when initializing the Hono app, pass the types of env as a generic
 const app = new Hono<{
 	Bindings: {
 		DATABASE_URL: string,
@@ -11,12 +10,24 @@ const app = new Hono<{
 	}
 }>();
 
-app.get('/api/v1/signup',async (c) => {
+app.get('/api/v1/all',async(c)=>{
+    const prisma = new PrismaClient({           //this from line 19-21, we have to write in every routes, because we cannot access the env variables from outside.. as everything we get is from this 'c' variable only.
+      datasourceUrl: c.env?.DATABASE_URL,        
+    }).$extends(withAccelerate())
+
+    const data=await prisma.user.findMany()
+
+    return c.json({
+      result:data
+    })
+})
+   
+app.post('/api/v1/signup',async (c) => {
     const prisma = new PrismaClient({
       datasourceUrl: c.env?.DATABASE_URL,        //c is the super power in honoJS
     }).$extends(withAccelerate())
 
-    const body=await c.req.json()           //remember app.use(express.json()), this line serves the same purpose
+    const body=await c.req.json()           //remember, we use to do await, while getting the inputs while working in the express as well, same this we are doing here, syntactically different.
     try {
       const user = await prisma.user.create({
         data: {
@@ -30,7 +41,6 @@ app.get('/api/v1/signup',async (c) => {
     } catch(e) {
       return c.status(403);
     }
-
 })
 
 app.post('/api/v1/signin',async (c) => {
@@ -57,15 +67,26 @@ app.post('/api/v1/signin',async (c) => {
     return c.json({token})
 })
 
+app.post('/api/v1/blog',async (c) => {
+    const prisma = new PrismaClient({
+      datasourceUrl: c.env?.DATABASE_URL,        
+    }).$extends(withAccelerate())
+    
+    const body=await c.req.json()           //remember, we use to do await, while getting the inputs while working in the express as well, same this we are doing here, syntactically different.
+})
+
 app.get('/api/v1/blog/:id', (c) => {
+
 	const id = c.req.param('id')
 	console.log(id);
 	return c.text('get blog route')
 })
 
-app.post('/api/v1/blog', (c) => {
 
-	return c.text('signin route')
+app.get('/api/v1/blog/bulk',async(c)=>{
+    const prisma = new PrismaClient({
+      datasourceUrl: c.env?.DATABASE_URL,        
+    }).$extends(withAccelerate())
 })
 
 app.put('/api/v1/blog', (c) => {
