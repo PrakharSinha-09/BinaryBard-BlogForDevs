c5f3214eb0eb837f64ec0304b8c2845f5843777e
Prakhar Sinha <prakhar.sinha2k2@gmail.com>
2024-03-26 12:56:24 +0530

    update: added properties in zod inference for the auth in common package

--- common/src/index.ts
+++ common/src/index.ts
@@ -3,7 +3,11 @@ import z from 'zod'
 export const signupInput=z.object({
     name: z.string().optional(),
     email: z.string().email(),
-    password: z.string().min(6)
+    password: z.string().min(6),
+    bio: z.string().optional(),
+    twitter: z.string().optional(),
+    linkedin: z.string().optional(),
+    github : z.string().optional(),
 })
 
 export const signinInput=z.object({
